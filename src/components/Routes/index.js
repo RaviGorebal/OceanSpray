aa2e52ca0ce50dcd9a2ca85d01731cfcfc0833f9
RaviGorebal <veerabrahmam.nuthalapati@stellapps.com>
2019-06-05 18:57:22 +0530

    Tabs added

--- src/components/Routes/index.js
+++ src/components/Routes/index.js
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import React, {Component} from "react";
-import {BackHandler} from "react-native";
+import {BackHandler, Image} from "react-native";
 import {Router, Scene} from "react-native-router-flux";
 
 import  MembershipDetails  from "../../screens/MemberShipDetails/MembershipDetails";
@@ -19,24 +19,67 @@ const defaultProps = {
     isLoggedin: false
 };
 
+
+
 export default class Routes extends Component {
+
+    constructor(props) {
+        super(props);
+        this.renderHomeIcon = this.renderHomeIcon.bind(this);
+        this.renderMyHarvestIcon = this.renderMyHarvestIcon.bind(this);
+        this.renderMyFarmIcon = this.renderMyFarmIcon.bind(this);
+        this.renderCommunityIcon = this.renderCommunityIcon.bind(this);
+        this.rendermoreIcon = this.rendermoreIcon.bind(this);
+}
 
     handleBackButton() {
         BackHandler.exitApp();
         return true;
     }
 
+    renderHomeIcon({focused})  {
+        let image = focused ? require('../../assets/Home.png') : require('../../assets/Home.png');
+        return ( <Image source={image} /> );
+    }
+
+    renderMyHarvestIcon({focused})  {
+        let image = focused ? require('../../assets/MyHarvest.png') : require('../../assets/MyHarvest.png');
+        return ( <Image source={image} /> );
+    }
+
+    renderMyFarmIcon({focused})  {
+        let image = focused ? require('../../assets/MyFarm.png') : require('../../assets/MyFarm.png');
+        return ( <Image source={image} /> );
+    }
+
+    renderCommunityIcon({focused})  {
+        let image = focused ? require('../../assets/Community.png') : require('../../assets/Community.png');
+        return ( <Image source={image} /> );
+    }
+
+    rendermoreIcon({focused})  {
+        let image = focused ? require('../../assets/more.png') : require('../../assets/more.png');
+        return ( <Image source={image} /> );
+    }
+
     render() {
         const {isLoggedin} = this.props;
         return (
             <Router {...this.props}  backAndroidHandler={this.handleBackButton.bind(this)}>
-                <Scene hideNavBar={true} initial={true}>
-                <Scene key="myHarvestFilterContainer" component={MyHarvestSortAndFilterContainer} title="MyHarvest filter"/>    
-                <Scene key="membershipDetails" component={MembershipDetails} title="MembershipDetails" />
-                    <Scene key="languageContainer" component={LanguageContainer} title="LanguageContainer" />
-                    <Scene key="loginContainer" component={LoginContainer} title="Login" />
-                    <Scene key="forgotUsername" component={ForgotUserNameContainer} title="Forgot Username" />
-                    <Scene key="forgotPassword" component={ForgotPasswordContainer} title="Forgot Password" />
+                <Scene initial={true}>
+                           <Scene tabs  key="Home"  hideNavBar={true}  tabBarPosition="bottom" swipeEnabled={true} tabBarStyle={{height: 65, backgroundColor:'#ffffff', paddingVertical: 4}}>
+                                 <Scene key="Home" hideNavBar={true}  icon={this.renderHomeIcon} component={LanguageContainer} />
+                                 <Scene key="My Harvest"  hideNavBar={true}  icon={this.renderMyHarvestIcon} component={MembershipDetails}  />
+                                 <Scene key="My Farm" hideNavBar={true}  icon={this.renderMyFarmIcon} component={LoginContainer}  />
+                                 <Scene key="Community" hideNavBar={true}  icon={this.renderCommunityIcon} component={ForgotUserNameContainer}  />
+                                 <Scene key="More" hideNavBar={true}  icon={this.rendermoreIcon} component={ForgotPasswordContainer} />
+                            </Scene>
+                <Scene key="myHarvestFilterContainer" hideNavBar={true} component={MyHarvestSortAndFilterContainer} title="MyHarvest filter"/>    
+                <Scene key="membershipDetails" hideNavBar={true} component={MembershipDetails} title="MembershipDetails" />
+                    <Scene key="languageContainer" hideNavBar={true} component={LanguageContainer} title="LanguageContainer" />
+                    <Scene key="loginContainer" hideNavBar={true} component={LoginContainer} title="Login" />
+                    <Scene key="forgotUsername" hideNavBar={true} component={ForgotUserNameContainer} title="Forgot Username" />
+                    <Scene key="forgotPassword" hideNavBar={true} component={ForgotPasswordContainer} title="Forgot Password" />
                 </Scene>
             </Router>
         );
